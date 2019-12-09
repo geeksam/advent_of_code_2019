b4024d23ed31c7e934f6efdcc2ec950efdbfbd91
Sam Livingston-Gray <geeksam@gmail.com>
2019-12-09 14:26:38 -0800

    Add some basic rspec behavior

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,10 @@ LIB = Pathname.new(File.dirname(__FILE__)).join("../lib")
 
 require 'pp'
 
+RSpec.configure do |c|
+  c.filter_run_when_matching :focus
+
+  c.after :example do
+    $debug = false
+  end
+end
