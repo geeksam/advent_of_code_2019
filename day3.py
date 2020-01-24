3dbe7b35d5084621cda55c9578947ce277cc1d71
Sam Livingston-Gray <geeksam@gmail.com>
2020-01-23 17:22:04 -0800

    hacked a bit more on day 3 in python

--- day3.py
+++ day3.py
@@ -1,34 +1,47 @@
 import math
 import re
 
+class Vector:
+    def __init__(self, dir, len):
+        self.dir, self.len = dir, len
+
+    @classmethod
+    def build(self, vector_or_string):
+        if isinstance(vector_or_string, str):
+            m = re.match(r"([URDL])(\d+)", vector_or_string)
+            dir = m.group(1)
+            len = int(m.group(2))
+            return self(dir, len)
+        if isinstance(vector_or_string, self):
+            return vector_or_string
+        else:
+            raise "IDKWTF"
+
+    def unit(self):
+        if self.dir == "U": return Point(  0,  1 )
+        if self.dir == "R": return Point(  1,  0 )
+        if self.dir == "D": return Point(  0, -1 )
+        if self.dir == "L": return Point( -1,  0 )
+        else:           raise "IDKWTF"
+
+    def to_point(self):
+        return self.unit() * self.len
+
+    def __eq__(self, other):
+        return self.dir == other.dir and self.len == other.len
+
+    def __repr__(self):
+        return f"Vector('{self.dir}{self.len}')"
+
 class Point:
     def __init__(self, x, y):
         self.x, self.y = x, y
 
-    @classmethod
-    def from_vector(self, vector_string):
-        p = re.compile('([URDL])([0-9]+)')
-        m = p.match(vector_string)
-        if m:
-            direction = self.from_direction(m.group(1))
-            magnitude = int(m.group(2))
-            return direction * magnitude
-        else:
-            return self(0, 0)
-
-    @classmethod
-    def from_direction(self, direction):
-        if   "U" == direction: return self(  0,  1 )
-        elif "D" == direction: return self(  0, -1 )
-        elif "L" == direction: return self( -1,  0 )
-        elif "R" == direction: return self(  1,  0 )
-        else:                  return self(  0,  0 )
+    def manhattan_distance(self):
+        return math.fabs(self.x) + math.fabs(self.y)
 
     def __repr__(self):
         return f"Point({self.x}, {self.y})"
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
 
     def __add__(self, other):
         x = self.x + other.x
@@ -36,28 +49,36 @@ class Point:
         return self.__class__(x, y)
 
     def __mul__(self, other):
+        if isinstance(other, int):
+            return self.__class__(self.x * other, self.y * other)
         if isinstance(other, self.__class__):
-            x = self.x * other.x
-            y = self.y * other.y
+            return self.__class__(self.x * other.x, self.y * other.y)
         else:
-            x = self.x * other
-            y = self.y * other
-        return self.__class__(x, y)
+            raise "IDKWTF"
 
-    def manhattan_distance(self):
-        return math.fabs(self.x) + math.fabs(self.y)
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
 
     def points_along(self, vector_string):
-        pass
+        v = Vector.build(vector_string)
+        points = []
+        for i in range(0, v.len+1):
+            delta = v.unit() * i
+            p = self + delta
+            points.append(p)
+        return points
 
 class Path:
     def __init__(self):
-        self.junctions = []
-        self.points = [ Point(0, 0) ]
+        self.vectors = []
 
-    def extend(self, vector_string):
-        # p1 = self.junctions.last or Point(0, 0)
-        p2 = Point.from_vector(vector_string)
-        self.junctions.append(p2)
+    def extend(self, vector_or_string):
+        v = Vector.build(vector_or_string)
+        self.vectors.append(v)
 
-
+    def points(self):
+        list = [ Point(0, 0) ]
+        for v in self.vectors:
+            tail = list[-1]
+            list += tail.points_along(v)[1:]
+        return list
--- test/day3_test.py
+++ test/day3_test.py
@@ -1,70 +1,90 @@
 from ..day3 import *
 
+class TestVector:
+    def test_construction(self):
+        v = Vector("R", 3)
+        assert v.dir == "R"
+        assert v.len == 3
+
+    def test_for_using_string(self):
+        v = Vector.build("R3")
+        assert v.dir == "R"
+        assert v.len == 3
+
+    def test_for_using_vector(self):
+        v1 = Vector.build("R3")
+        v2 = Vector.build(v1)
+        assert v2.dir == "R"
+        assert v2.len == 3
+
+    def test_unit(self):
+        assert Point(  0,  1 ) == Vector.build("U2").unit()
+        assert Point(  1,  0 ) == Vector.build("R3").unit()
+        assert Point(  0, -1 ) == Vector.build("D4").unit()
+        assert Point( -1,  0 ) == Vector.build("L5").unit()
+
+    def test_to_point(self):
+        assert Point(2, 0) == Vector.build("R2").to_point()
+
+    def test_equality(self):
+        v1 = Vector("R", 2)
+        v2 = Vector("R", 2)
+        assert v1 == v2
+
 class TestPoint:
     def test_construction(self):
-        p = Point(0, 1)
-        assert 0 == p.x
-        assert 1 == p.y
-
-    def test_points_can_be_added(self):
-        p1 = Point(1, 1)
-        p2 = Point(2, 3)
-        assert p1 + p2 == Point(3, 4)
-
-    def test_points_can_be_scaled_by_a_scalar(self):
-        p1 = Point(1, 2)
-        assert Point(3, 6) == p1 * 3
-
-    def test_points_can_be_scaled_by_a_point(self):
-        p1 = Point(1, 2)
-        p2 = Point(3, 2)
-        assert Point(3, 4) == p1 * p2
-
-    def test_manhattan_distance(self):
-        p1 = Point(2, 3)
-        assert 5 == p1.manhattan_distance()
-
-    def test_construction_from_direction(self):
-        assert Point( 0,  1) == Point.from_direction("U")
-        assert Point( 1,  0) == Point.from_direction("R")
-        assert Point( 0, -1) == Point.from_direction("D")
-        assert Point(-1,  0) == Point.from_direction("L")
+        p = Point(2, 3)
+        assert 2 == p.x
+        assert 3 == p.y
 
     def test_construction_from_vector(self):
-        assert Point( 0,  1) == Point.from_vector("U1")
-        assert Point( 1,  0) == Point.from_vector("R1")
-        assert Point( 0, -1) == Point.from_vector("D1")
-        assert Point(-1,  0) == Point.from_vector("L1")
+        v = Vector.build("R2")
+        assert Point(2, 0) == v.to_point()
 
-        assert Point( 0,  1) == Point.from_vector("U1")
-        assert Point( 2,  0) == Point.from_vector("R2")
-        assert Point( 0, -3) == Point.from_vector("D3")
-        assert Point(-4,  0) == Point.from_vector("L4")
+    def test_manhattan_distance(self):
+        p = Point(2, 3)
+        assert 5 == p.manhattan_distance()
+
+    def test_add_and_eql(self):
+        p1 = Point(1, 2)
+        p2 = Point(3, 4)
+        assert Point(4, 6) == (p1 + p2)
+
+    def test_mul_point_and_int(self):
+        p = Point(2, 3)
+        assert Point(4, 6) == (p * 2)
+
+    def test_mul_two_points(self):
+        p1 = Point(1, 2)
+        p2 = Point(3, 4)
+        assert Point(3, 8) == (p1 * p2)
 
     def test_points_along(self):
-        p1 = Point(1, 1)
-        assert p1.points_along("R2") == [
-            Point(1, 1),
-            Point(2, 1),
-            Point(3, 1),
+        p = Point(1, 2)
+        assert p.points_along("R3") == [
+            Point(1, 2),
+            Point(2, 2),
+            Point(3, 2),
+            Point(4, 2),
         ]
 
 class TestPath:
-    def test_empty_path_has_no_junctions(self):
-        assert Path().junctions == []
+    def test_empty_path_has_no_vectors(self):
+        p = Path()
+        assert p.vectors == []
 
-    def test_empty_path_points_contains_the_origin(self):
-        assert Path().points == [ Point(0, 0) ]
+    def test_extend(self):
+        p = Path()
+        p.extend("R2")
+        assert p.vectors == [ Vector("R", 2) ]
+        p.extend("U1")
+        assert p.vectors == [ Vector("R", 2), Vector("U", 1) ]
 
-    def test_path_can_add_junctions(self):
-        path = Path()
-        path.extend("R3")
-        assert path.junctions == [ Point(3, 0) ]
+    def test_points(self):
+        p = Path()
+        print(p.points())
+        assert p.points() == [ Point(0, 0) ]
+        p.extend("R2")
+        print(p.points())
+        assert p.points() == [ Point(0, 0), Point(1, 0), Point(2, 0) ]
 
-    # def test_path_with_junctions_knows_all_points_on_path(self):
-    #     path = Path()
-    #     path.extend("R3")
-    #     assert path.points == [
-    #         Point(0, 0)
-    #     ]
-    #
