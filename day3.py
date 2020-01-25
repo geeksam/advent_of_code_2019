6c2d1f1ec752b5ada9d004e729bcb50c41ed7384
Sam Livingston-Gray <geeksam@gmail.com>
2020-01-24 17:10:41 -0800

    python version passing all but one (very slow) test

--- day3.py
+++ day3.py
@@ -1,41 +1,56 @@
 import math
 import re
 
-class Vector:
-    def __init__(self, dir, len):
-        self.dir, self.len = dir, len
+def distance_to_closest_intersection(path1, path2):
+    ints = path1.intersections(path2)
+    dists = map(lambda path: path.manhattan_distance(), ints)
+    return min(dists)
 
-    @classmethod
-    def build(self, vector_or_string):
+def steps_to_fastest_intersection(path1, path2):
+    distance = lambda pt: [ path1.steps_to(pt) + path2.steps_to(pt), pt ]
+    ints = path1.intersections(path2)
+    list_with_steps = list(map(distance, ints))
+    list_with_steps.sort(key=lambda x: x[0])
+    steps, _point = list_with_steps[0]
+    return steps
+
+class Vector:
+    def __init__(self, vector_or_string):
         if isinstance(vector_or_string, str):
             m = re.match(r"([URDL])(\d+)", vector_or_string)
-            dir = m.group(1)
-            len = int(m.group(2))
-            return self(dir, len)
-        if isinstance(vector_or_string, self):
-            return vector_or_string
+            direction = m.group(1)
+            length = int(m.group(2))
+            self.direction, self.length = direction, length
+        elif isinstance(vector_or_string, self.__class__):
+            self.direction = vector_or_string.direction
+            self.length    = vector_or_string.length
         else:
             raise "IDKWTF"
 
     def unit(self):
-        if self.dir == "U": return Point(  0,  1 )
-        if self.dir == "R": return Point(  1,  0 )
-        if self.dir == "D": return Point(  0, -1 )
-        if self.dir == "L": return Point( -1,  0 )
-        else:           raise "IDKWTF"
+        if self.direction == "U": return Point(  0,  1 )
+        if self.direction == "R": return Point(  1,  0 )
+        if self.direction == "D": return Point(  0, -1 )
+        if self.direction == "L": return Point( -1,  0 )
+        raise "IDKWTF"
 
     def to_point(self):
-        return self.unit() * self.len
+        return self.unit() * self.length
 
     def __eq__(self, other):
-        return self.dir == other.dir and self.len == other.len
+        return self.direction == other.direction and self.length == other.length
 
     def __repr__(self):
-        return f"Vector('{self.dir}{self.len}')"
+        return f"Vector('{self.direction}{self.length}')"
 
 class Point:
     def __init__(self, x, y):
         self.x, self.y = x, y
+
+    @classmethod
+    def from_vector(cls, vector):
+        v = Vector(vector)
+        return v.to_point()
 
     def manhattan_distance(self):
         return math.fabs(self.x) + math.fabs(self.y)
@@ -44,41 +59,70 @@ class Point:
         return f"Point({self.x}, {self.y})"
 
     def __add__(self, other):
-        x = self.x + other.x
-        y = self.y + other.y
-        return self.__class__(x, y)
+        if isinstance(other, self.__class__):
+            x = self.x + other.x
+            y = self.y + other.y
+            return self.__class__(x, y)
+        if isinstance(other, Vector):
+            p = self.__class__.from_vector(other)
+            return self + p
+        raise "IDKWTF"
 
     def __mul__(self, other):
         if isinstance(other, int):
             return self.__class__(self.x * other, self.y * other)
         if isinstance(other, self.__class__):
             return self.__class__(self.x * other.x, self.y * other.y)
-        else:
-            raise "IDKWTF"
+        raise "IDKWTF"
 
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
     def points_along(self, vector_string):
-        v = Vector.build(vector_string)
+        v = Vector(vector_string)
         points = []
-        for i in range(0, v.len+1):
+        for i in range(0, v.length+1):
             delta = v.unit() * i
             p = self + delta
             points.append(p)
         return points
 
+    def __hash__(self):
+        return hash((self.x, self.y))
+
 class Path:
-    def __init__(self):
+    def __init__(self, path_string = None):
         self.vectors = []
+        if isinstance(path_string, str):
+            for vector_string in path_string.split(","):
+                self.extend(vector_string)
 
     def extend(self, vector_or_string):
-        v = Vector.build(vector_or_string)
+        v = Vector(vector_or_string)
         self.vectors.append(v)
 
     def points(self):
-        list = [ Point(0, 0) ]
+        pts = [ Point(0, 0) ]
         for v in self.vectors:
-            tail = list[-1]
-            list += tail.points_along(v)[1:]
-        return list
+            tail = pts[-1]
+            pts += tail.points_along(v)[1:]
+        return pts
+
+    def turning_points(self):
+        pts = [ Point(0, 0) ]
+        for v in self.vectors:
+            pts.append( pts[-1] + v )
+        return pts
+
+    def __repr__(self):
+        tps = map(str, self.turning_points())
+        return " --> ".join(tps)
+
+    def steps_to(self, point):
+        return self.points().index(point)
+
+    def intersections(self, other):
+        ps1 = set( self.points() )
+        ps2 = set( other.points() )
+        ints = ps1 & ps2
+        return [ x for x in self.points() if x in ints and x != Point(0, 0) ]
--- test/day3_test.py
+++ test/day3_test.py
@@ -1,54 +1,66 @@
 from ..day3 import *
 
+
 class TestVector:
     def test_construction(self):
-        v = Vector("R", 3)
-        assert v.dir == "R"
-        assert v.len == 3
+        v = Vector("R3")
+        assert v.direction == "R"
+        assert v.length == 3
 
     def test_for_using_string(self):
-        v = Vector.build("R3")
-        assert v.dir == "R"
-        assert v.len == 3
+        v = Vector("R3")
+        assert v.direction == "R"
+        assert v.length == 3
 
     def test_for_using_vector(self):
-        v1 = Vector.build("R3")
-        v2 = Vector.build(v1)
-        assert v2.dir == "R"
-        assert v2.len == 3
+        v1 = Vector("R3")
+        v2 = Vector(v1)
+        assert v2.direction == "R"
+        assert v2.length == 3
 
     def test_unit(self):
-        assert Point(  0,  1 ) == Vector.build("U2").unit()
-        assert Point(  1,  0 ) == Vector.build("R3").unit()
-        assert Point(  0, -1 ) == Vector.build("D4").unit()
-        assert Point( -1,  0 ) == Vector.build("L5").unit()
+        assert Point(0, 1) == Vector("U2").unit()
+        assert Point(1, 0) == Vector("R3").unit()
+        assert Point(0, -1) == Vector("D4").unit()
+        assert Point(-1, 0) == Vector("L5").unit()
 
     def test_to_point(self):
-        assert Point(2, 0) == Vector.build("R2").to_point()
+        assert Point(2, 0) == Vector("R2").to_point()
 
     def test_equality(self):
-        v1 = Vector("R", 2)
-        v2 = Vector("R", 2)
+        v1 = Vector("R2")
+        v2 = Vector("R2")
         assert v1 == v2
+
 
 class TestPoint:
     def test_construction(self):
         p = Point(2, 3)
-        assert 2 == p.x
-        assert 3 == p.y
+        assert p.x == 2
+        assert p.y == 3
 
     def test_construction_from_vector(self):
-        v = Vector.build("R2")
+        v = Vector("R2")
         assert Point(2, 0) == v.to_point()
 
     def test_manhattan_distance(self):
         p = Point(2, 3)
-        assert 5 == p.manhattan_distance()
+        assert p.manhattan_distance() == 5
 
-    def test_add_and_eql(self):
+    def test_eql(self):
+        assert Point(1, 2) != Point(2, 2)
+        assert Point(1, 2) != Point(1, 1)
+        assert Point(1, 2) == Point(1, 2)
+
+    def test_add_two_points(self):
         p1 = Point(1, 2)
         p2 = Point(3, 4)
         assert Point(4, 6) == (p1 + p2)
+
+    def test_add_point_and_vector(self):
+        p = Point(1, 2)
+        v = Vector("U4")
+        assert Point(1, 6) == (p + v)
 
     def test_mul_point_and_int(self):
         p = Point(2, 3)
@@ -68,6 +80,7 @@ class TestPoint:
             Point(4, 2),
         ]
 
+
 class TestPath:
     def test_empty_path_has_no_vectors(self):
         p = Path()
@@ -76,15 +89,99 @@ class TestPath:
     def test_extend(self):
         p = Path()
         p.extend("R2")
-        assert p.vectors == [ Vector("R", 2) ]
+        assert p.vectors == [Vector("R2")]
         p.extend("U1")
-        assert p.vectors == [ Vector("R", 2), Vector("U", 1) ]
+        assert p.vectors == [Vector("R2"), Vector("U1")]
 
     def test_points(self):
         p = Path()
-        print(p.points())
-        assert p.points() == [ Point(0, 0) ]
+        assert p.points() == [Point(0, 0)]
         p.extend("R2")
-        print(p.points())
-        assert p.points() == [ Point(0, 0), Point(1, 0), Point(2, 0) ]
+        assert p.points() == [Point(0, 0), Point(1, 0), Point(2, 0)]
+        p.extend("U1")
+        p.extend("L1")
+        p.extend("D1")
+        assert p.points() == [
+            Point(0, 0),
+            Point(1, 0),
+            Point(2, 0),
+            Point(2, 1),
+            Point(1, 1),
+            Point(1, 0),
+        ]
 
+    def test_construction_from_string(self):
+        p = Path("R2,U1,L1,D1")
+        assert p.points() == [
+            Point(0, 0),
+            Point(1, 0),
+            Point(2, 0),
+            Point(2, 1),
+            Point(1, 1),
+            Point(1, 0),
+        ]
+
+    def test_turning_points(self):
+        p = Path("R2,U1,L1,D1")
+        assert p.turning_points() == [
+            Point(0, 0),
+            Point(2, 0),
+            Point(2, 1),
+            Point(1, 1),
+            Point(1, 0),
+        ]
+
+    def test_string_representation(self):
+        p = Path("R2,U1,L1,D1")
+        assert (
+            str(p)
+            == "Point(0, 0) --> Point(2, 0) --> Point(2, 1) --> Point(1, 1) --> Point(1, 0)"
+        )
+
+    def test_steps_to(self):
+        p = Path("R2,D2")
+        assert p.steps_to(Point(2, -1)) == 3
+
+    def test_intersections(self):
+        path1 = Path("R8,U5,L5,D3")
+        path2 = Path("U7,R6,D4,L4")
+        ints = path1.intersections(path2)
+        assert ints == [Point(6, 5), Point(3, 3)]
+
+
+def test_part_one_example_one():
+    path1 = Path("R8,U5,L5,D3")
+    path2 = Path("U7,R6,D4,L4")
+    assert distance_to_closest_intersection(path1, path2) == 6
+
+
+def test_part_one_example_two():
+    path1 = Path( "R75,D30,R83,U83,L12,D49,R71,U7,L72" )
+    path2 = Path( "U62,R66,U55,R34,D71,R55,D58,R83" )
+    assert 159 == distance_to_closest_intersection(path1, path2)
+
+def test_part_one_example_three():
+    path1 = Path( "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" )
+    path2 = Path( "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" )
+    assert 135 == distance_to_closest_intersection(path1, path2)
+
+# def test_part_one_answer():
+#     path1 = Path( "R999,D666,L86,U464,R755,U652,R883,D287,L244,U308,L965,U629,R813,U985,R620,D153,L655,D110,R163,D81,L909,D108,L673,D165,L620,U901,R601,D561,L490,D21,R223,U478,R80,U379,R873,U61,L674,D732,R270,U297,L354,U264,L615,D2,R51,D582,R280,U173,R624,U644,R451,D97,R209,U245,R32,U185,R948,D947,R380,D945,L720,U305,R911,U614,L419,D751,L934,U371,R291,D166,L137,D958,R368,U441,R720,U822,R961,D32,R242,D972,L782,D166,L680,U111,R379,D155,R213,U573,R761,D543,R762,U953,R317,U841,L38,U900,R573,U766,R807,U950,R945,D705,R572,D994,L633,U33,L173,U482,R253,D835,R800,U201,L167,U97,R375,D813,L468,D924,L972,U570,R975,D898,L195,U757,L565,D378,R935,U4,L334,D707,R958,U742,R507,U892,R174,D565,L862,D311,L770,D619,L319,D698,L169,D652,L761,D644,R837,U43,L197,D11,L282,D345,L551,U460,R90,D388,R911,U602,L21,D275,L763,U880,R604,D838,R146,U993,L99,U99,R928,U54,L148,D863,R618,U449,R549,D659,R449,D435,L978,D612,L645,D691,R190,D434,L841,D364,L634,D590,R962,U15,R921,D442,L284,U874,R475,D556,L135,U376,L459,D673,L515,U438,L736,U266,L601,U351,R496,U891,L893,D597,L135,D966,R121,U763,R46,D110,R830,U644,L932,D122,L123,U145,R273,U690,L443,D372,R818,D259,L695,U69,R73,D718,R106,U929,L346,D291,L857,D341,R297,D823,R819,U496,L958,U394,R102,D763,L444,D835,L33,U45,R812,U845,R196,U458,R231,U637,R661,D983,L941,U975,L353,U609,L698,U152,R122,D882,R682,D926,R729,U429,R255,D227,R987,D547,L446,U217,R678,D464,R849,D472,L406,U940,L271,D779,R980,D751,L171,D420,L49,D271,R430,D530,R509,U479,R135,D770,R85,U815,R328,U234,R83" )
+#     path2 = Path( "L1008,D951,L618,U727,L638,D21,R804,D19,L246,U356,L51,U8,L627,U229,R719,D198,L342,U240,L738,D393,L529,D22,R648,D716,L485,U972,L580,U884,R612,D211,L695,U731,R883,U470,R732,U723,R545,D944,R18,U554,L874,D112,R782,D418,R638,D296,L123,U426,L479,U746,L209,D328,L121,D496,L172,D228,L703,D389,R919,U976,R364,D468,L234,U318,R912,U236,R148,U21,R26,D116,L269,D913,L949,D206,L348,U496,R208,U706,R450,U472,R637,U884,L8,U82,L77,D737,L677,D358,L351,U719,R154,U339,L506,U76,L952,D791,L64,U879,R332,D244,R638,D453,L107,D908,L58,D188,R440,D147,R913,U298,L681,D582,L943,U503,L6,U459,L289,D131,L739,D443,R333,D138,R553,D73,L475,U930,L332,U518,R614,D553,L515,U602,R342,U95,R131,D98,R351,U921,L141,U207,R199,U765,R55,U623,R768,D620,L722,U31,L891,D862,R85,U271,R590,D184,R960,U149,L985,U82,R591,D384,R942,D670,R584,D637,L548,U844,R353,U496,L504,U3,L830,U239,R246,U279,L146,U965,R784,U448,R60,D903,R490,D831,L537,U109,R271,U306,L342,D99,L234,D936,R621,U870,R56,D29,R366,D562,R276,D134,L289,D425,R597,D102,R276,D600,R1,U322,L526,D744,L259,D111,R994,D581,L973,D871,R173,D924,R294,U478,R384,D242,R606,U629,R472,D651,R526,U55,R885,U637,R186,U299,R812,D95,R390,D689,R514,U483,R471,D591,L610,D955,L599,D674,R766,U834,L417,U625,R903,U376,R991,U175,R477,U524,L453,D407,R72,D217,L968,D892,L806,D589,R603,U938,L942,D940,R578,U820,L888,U232,L740,D348,R445,U269,L170,U979,L159,U433,L31,D818,L914,U600,L33,U159,R974,D983,L922,U807,R682,U525,L234,U624,L973,U123,L875,D64,L579,U885,L911,D578,R17,D293,L211" )
+#     assert 1983 == distance_to_closest_intersection(path1, path2)
+
+def test_part_two_example_one():
+    path1 = Path( "R75,D30,R83,U83,L12,D49,R71,U7,L72" )
+    path2 = Path( "U62,R66,U55,R34,D71,R55,D58,R83" )
+    assert steps_to_fastest_intersection(path1, path2) == 610
+
+def test_part_two_example_two():
+    path1 = Path( "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51" )
+    path2 = Path( "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7" )
+    assert steps_to_fastest_intersection(path1, path2) == 410
+
+# FIXME: this example (a) takes 30s to run and (b) produces the wrong answer
+# def test_part_two_answer():
+#     path1 = Path( "R999,D666,L86,U464,R755,U652,R883,D287,L244,U308,L965,U629,R813,U985,R620,D153,L655,D110,R163,D81,L909,D108,L673,D165,L620,U901,R601,D561,L490,D21,R223,U478,R80,U379,R873,U61,L674,D732,R270,U297,L354,U264,L615,D2,R51,D582,R280,U173,R624,U644,R451,D97,R209,U245,R32,U185,R948,D947,R380,D945,L720,U305,R911,U614,L419,D751,L934,U371,R291,D166,L137,D958,R368,U441,R720,U822,R961,D32,R242,D972,L782,D166,L680,U111,R379,D155,R213,U573,R761,D543,R762,U953,R317,U841,L38,U900,R573,U766,R807,U950,R945,D705,R572,D994,L633,U33,L173,U482,R253,D835,R800,U201,L167,U97,R375,D813,L468,D924,L972,U570,R975,D898,L195,U757,L565,D378,R935,U4,L334,D707,R958,U742,R507,U892,R174,D565,L862,D311,L770,D619,L319,D698,L169,D652,L761,D644,R837,U43,L197,D11,L282,D345,L551,U460,R90,D388,R911,U602,L21,D275,L763,U880,R604,D838,R146,U993,L99,U99,R928,U54,L148,D863,R618,U449,R549,D659,R449,D435,L978,D612,L645,D691,R190,D434,L841,D364,L634,D590,R962,U15,R921,D442,L284,U874,R475,D556,L135,U376,L459,D673,L515,U438,L736,U266,L601,U351,R496,U891,L893,D597,L135,D966,R121,U763,R46,D110,R830,U644,L932,D122,L123,U145,R273,U690,L443,D372,R818,D259,L695,U69,R73,D718,R106,U929,L346,D291,L857,D341,R297,D823,R819,U496,L958,U394,R102,D763,L444,D835,L33,U45,R812,U845,R196,U458,R231,U637,R661,D983,L941,U975,L353,U609,L698,U152,R122,D882,R682,D926,R729,U429,R255,D227,R987,D547,L446,U217,R678,D464,R849,D472,L406,U940,L271,D779,R980,D751,L171,D420,L49,D271,R430,D530,R509,U479,R135,D770,R85,U815,R328,U234,R83" )
+#     path2 = Path( "L1008,D951,L618,U727,L638,D21,R804,D19,L246,U356,L51,U8,L627,U229,R719,D198,L342,U240,L738,D393,L529,D22,R648,D716,L485,U972,L580,U884,R612,D211,L695,U731,R883,U470,R732,U723,R545,D944,R18,U554,L874,D112,R782,D418,R638,D296,L123,U426,L479,U746,L209,D328,L121,D496,L172,D228,L703,D389,R919,U976,R364,D468,L234,U318,R912,U236,R148,U21,R26,D116,L269,D913,L949,D206,L348,U496,R208,U706,R450,U472,R637,U884,L8,U82,L77,D737,L677,D358,L351,U719,R154,U339,L506,U76,L952,D791,L64,U879,R332,D244,R638,D453,L107,D908,L58,D188,R440,D147,R913,U298,L681,D582,L943,U503,L6,U459,L289,D131,L739,D443,R333,D138,R553,D73,L475,U930,L332,U518,R614,D553,L515,U602,R342,U95,R131,D98,R351,U921,L141,U207,R199,U765,R55,U623,R768,D620,L722,U31,L891,D862,R85,U271,R590,D184,R960,U149,L985,U82,R591,D384,R942,D670,R584,D637,L548,U844,R353,U496,L504,U3,L830,U239,R246,U279,L146,U965,R784,U448,R60,D903,R490,D831,L537,U109,R271,U306,L342,D99,L234,D936,R621,U870,R56,D29,R366,D562,R276,D134,L289,D425,R597,D102,R276,D600,R1,U322,L526,D744,L259,D111,R994,D581,L973,D871,R173,D924,R294,U478,R384,D242,R606,U629,R472,D651,R526,U55,R885,U637,R186,U299,R812,D95,R390,D689,R514,U483,R471,D591,L610,D955,L599,D674,R766,U834,L417,U625,R903,U376,R991,U175,R477,U524,L453,D407,R72,D217,L968,D892,L806,D589,R603,U938,L942,D940,R578,U820,L888,U232,L740,D348,R445,U269,L170,U979,L159,U433,L31,D818,L914,U600,L33,U159,R974,D983,L922,U807,R682,U525,L234,U624,L973,U123,L875,D64,L579,U885,L911,D578,R17,D293,L211" )
+#     assert steps_to_fastest_intersection(path1, path2) == 107647
